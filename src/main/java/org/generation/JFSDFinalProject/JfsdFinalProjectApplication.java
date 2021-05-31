0207a0ed7583ec8b258eab8ddad174e8482c7a00
edmundloo <81542420+halcyoned@users.noreply.github.com>
2021-05-31 21:46:59 +0800

    Initial commit

---
+++ src/main/java/org/generation/JFSDFinalProject/JfsdFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.generation.JFSDFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdFinalProjectApplication.class, args);
+	}
+
+}
